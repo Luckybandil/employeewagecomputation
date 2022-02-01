484d4e6853419203e9c5e185e71d72e8aedbcb5e
Lucky Bandil <96731082+Luckybandil@users.noreply.github.com>
2022-02-01 15:11:11 +0530

    Commit changed

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEmpWageComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Program"); 
+            WageComputation EmployeeWage = new WageComputation();
+            EmployeeWage.Attendance();
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ WageComputation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEmpWageComp
+{
+    public class WageComputation
+    {
+        const int Emp_Full_Time = 1;
+        const int Emp_Part_Time = 2;
+        public int Wage_Per_Hours = 20;
+        public int empHrs = 0;
+        public int Working_Days_Per_Month = 20;
+        public int Salary_Month = 100;
+        public int Working_Hours = 100;
+        public int Working_Days = 0;
+
+        public void Attendance()
+        {
+            while (empHrs < Working_Hours && Working_Days < Working_Days_Per_Month)
+            {
+                Working_Days++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case Emp_Full_Time:
+                        empHrs = 8;
+                        break;
+
+                    case Emp_Part_Time:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                Working_Hours += empHrs;
+
+            }
+            int EmpSalary = Working_Hours + Wage_Per_Hours;
+            Console.WriteLine(EmpSalary);
+        }
+    }
+
+}
+
+
+
+
